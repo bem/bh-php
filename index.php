26fc1d47aac18375a340fe36aef6ef63536a3b72
Alexej Yaroshevich <alex@qfox.ru>
2014-11-22 03:36:46 +0400

    add index.php for manual load

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "src/BH.php";
+require_once "src/Context.php";
+require_once "src/Json.php";
+require_once "src/JsonCollection.php";
+require_once "src/Mods.php";
+require_once "src/Step.php";
+require_once "src/WeakJsonParser.php";
